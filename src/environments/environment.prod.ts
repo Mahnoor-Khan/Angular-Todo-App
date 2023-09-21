8b0d4a3b7f535eaab8eba95611056081aebbc9c7
Mahnoor Khan <mahnoorza001@gmail.com>
2023-09-21 18:17:53 +0500

    Angular Todo App With firebase integration

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: true,
+    firebaseConfig :  {
+        apiKey: "AIzaSyDF68gYYdM-yuq0xyMyCYf3JJ5OzZewNaQ",
+        authDomain: "angularjs-todo-app-f2567.firebaseapp.com",
+        projectId: "angularjs-todo-app-f2567",
+        storageBucket: "angularjs-todo-app-f2567.appspot.com",
+        messagingSenderId: "886230454871",
+        appId: "1:886230454871:web:6ff48d8e492a1a488e8912"
+      }
+  };
+  
